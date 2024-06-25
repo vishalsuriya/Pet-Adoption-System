ca458e55eb5109daabd0f8e31bbf5e3565d66dff
sivab2211 <sivab2211@gmail.com>
2024-06-25 22:07:19 +0530

    First Commit

---
+++ Frontend/my-app/src/Components/Carousels.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Carousel from 'react-bootstrap/Carousel';
+
+function Carousels() {
+  return (
+
+ <Carousel controls={false} indicators={false} style={{ height: "500px", width: "100%" }} className="mt-1"> 
+      <Carousel.Item interval={2500}>
+      <img
+          src="https://i.pinimg.com/564x/30/6b/20/306b20828bd03a2f36a85d8fe99ff3ce.jpg"
+          className="d-block w-100 h-100"
+          alt="..."
+          style={{ maxHeight: "500px" }}
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={2500}>
+      <img
+          src="https://i.pinimg.com/564x/fa/ed/5c/faed5cd36e9d7622ad2d640b7a0d61cc.jpg"
+          className="d-block w-100"
+          alt="..."
+          style={{ maxHeight: "500px" }}
+        />
+      </Carousel.Item>
+      <Carousel.Item interval={2500}>
+      <img
+          src="https://i.pinimg.com/564x/06/ca/98/06ca985e66acefc2b0d35745ea27691c.jpg"
+          className="d-block w-100"
+          alt="..."
+          style={{ maxHeight: "500px" }}
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+
+export default Carousels;
---
+++ Frontend/my-app/src/Components/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const SearchBar= () => {
+  return (
+    <Container>
+      <Row className="justify-content-center">
+        <Col xs="auto">
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default SearchBar
