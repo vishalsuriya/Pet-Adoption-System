047093de20edf5462e721260a1f7c50f8b13e063
vishalsuriya <vishalsuriya2003@gmail.com>
2024-06-28 13:44:56 +0530

    Added image functionality

--- Backend/server.js
+++ Backend/server.js
@@ -2,12 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const multer = require("multer");
 const connectDB = require("./connection"); 
 const Pets = require("./Schemas/PetSchema"); 
-const fs = require('fs');
-const path = require('path');
-
+connectDB();
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,33 +12,26 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const uploadDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
-// Set up Multer for file handling
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Ensure this directory exists
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
+
+app.post('/api/adopts', async (req, res) => {
+  try {
+    const newAdopt = new Adopt({
+      ...req.body  
+    });
+
+    const savedAdopt = await newAdopt.save();  
+    res.status(200).json(savedAdopt);  
+  } catch (err) {
+    console.error('Error:', err);  
+    res.status(500).send('Internal Server Error');
+}
 });
 
-const upload = multer({ storage: storage });
-
-connectDB();
-
-app.post('/api/pets', upload.single('image'), async (req, res) => {
+app.post('/api/pets', async (req, res) => {
   try {
-    console.log('Request Body:', req.body);
-    console.log('Uploaded File:', req.file);
-
     const newPet = new Pets({
-      ...req.body,
-      image: req.file ? req.file.path : null 
+      ...req.body
     });
     const savedPet = await newPet.save();
     res.status(200).json(savedPet);
@@ -51,6 +41,12 @@ app.post('/api/pets', upload.single('image'), async (req, res) => {
   }
 });
 
+ app.get('/api/petdata',async(req,res)=>{
+  const data = await Pets.find();
+  res.send(data);
+ })
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
