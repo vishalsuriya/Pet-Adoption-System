3efaf008ed5c5e7edecf6abb65c2d4f69ac8dc9e
vishalsuriya <vishalsuriya2003@gmail.com>
2024-07-04 13:15:39 +0530

    Navigationbar1 edited

--- Frontend/my-app/src/Components/Navigationbar1.jsx
+++ Frontend/my-app/src/Components/Navigationbar1.jsx
@@ -1,53 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import Dropdown from "react-bootstrap/Dropdown";
 import Nav from "react-bootstrap/Nav";
-import {useEffect, useState} from 'react'
-import axios from 'axios'
+import axios from 'axios';
+
 const Navigationbar1 = () => {
+  const [pets, setPets] = useState([]);
 
-  const [pets, setPets] = useState([])
   useEffect(() => {
-      axios
-        .get("http://localhost:8000/api/petdata")
-        .then((response) => {
-          setPets(response.data);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }, []);
-
+    axios
+      .get("http://localhost:8000/api/petdata")
+      .then((response) => {
+        setPets(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
   return (
-    <Nav justify >
+    <Nav justify>
       <Nav.Item>
-        <div className="Paste-button">
-          <Dropdown className="bg-success">
-            <Dropdown.Toggle className="button bg-success border-success">Dogs and Puppies</Dropdown.Toggle>
-            <Dropdown.Menu className="dropdown-content">
-              {pets.map((data)=>{
-               return ( data.species === 'Dog' &&
-                            <Dropdown.Item>{data.breed}</Dropdown.Item>
-               )
-              })}
-              
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
+        <Dropdown className="bg-success">
+          <Dropdown.Toggle className="button bg-success border-success">Dogs and Puppies</Dropdown.Toggle>
+          <Dropdown.Menu className="dropdown-content">
+            {pets.filter(pet => pet.species === 'Dog').map((data, index) => (
+              <Dropdown.Item key={index} as={Link} to={`/dogs/${data.id}`}>
+                {data.breed}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </Nav.Item>
       <Nav.Item>
-        <div className="Paste-button">
-          <Dropdown className="bg-success">
-            <Dropdown.Toggle className="button bg-success border-success">Cats and Kittens</Dropdown.Toggle>
-            <Dropdown.Menu className="dropdown-content">
-            {pets.map((data)=>{
-               return ( data.species === 'Cat' &&
-                        <Dropdown.Item>{data.breed}</Dropdown.Item>
-               )
-              })}
-              
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
+        <Dropdown className="bg-success">
+          <Dropdown.Toggle className="button bg-success border-success">Cats and Kittens</Dropdown.Toggle>
+          <Dropdown.Menu className="dropdown-content">
+            {pets.filter(pet => pet.species === 'Cat').map((data, index) => (
+              <Dropdown.Item key={index} as={Link} to={`/cats/${data.id}`}>
+                {data.breed}
+              </Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
       </Nav.Item>
     </Nav>
   );
